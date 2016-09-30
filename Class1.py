a409451f561551d468998ec4ba8a7086f236c7b5
Ramsri Golla <Ramsri.Golla@intusurg.com>
2016-09-30 11:52:21 -0700

    Adding first class example

---
+++ Class1.py
@@ -0,0 +1,40 @@
+"""Object oriented Python- Class example."""
+import sys
+
+
+class Employee:
+    'Common base class for all employees'
+    empCount = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1
+
+    def displayCount(self):
+        print "Total Employee %d" % Employee.empCount
+
+    def displayEmployee(self):
+        print "Name : ", self.name, ", Salary: ", self.salary
+
+
+"This would create first object of Employee class"
+emp1 = Employee("Zara", 2000)
+"This would create second object of Employee class"
+emp2 = Employee("Manni", 5000)
+
+
+emp1.displayEmployee()
+emp2.displayEmployee()
+print "Total Employee %d" % Employee.empCount
+
+
+emp1.age = 7  # Add an 'age' attribute.
+
+print emp1.age
+
+print "Employee.__doc__:", Employee.__doc__
+print "Employee.__name__:", Employee.__name__
+print "Employee.__module__:", Employee.__module__
+print "Employee.__bases__:", Employee.__bases__
+print "Employee.__dict__:", Employee.__dict__
